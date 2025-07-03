d02c7cddf472438616f6905cdd73a2fcf8b54f23
Ahmed Gad <128515678+ahmadgaad@users.noreply.github.com>
2025-07-04 01:02:57 +0300

    Integrate Firebase Core with Flutter app
    - Added Firebase configuration files and initialized Firebase in the app.
    - Updated Gradle and plugin registrant files for Android, macOS, and Windows to support Firebase.
    - Added firebase_core dependency and refactored main.dart to initialize Firebase before running the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
+import 'package:docdoc/docdoc_app.dart';
+import 'package:docdoc/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const DocDocApp());
 }
